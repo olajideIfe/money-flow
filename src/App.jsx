3c7fbf7f02615c2b12d469aebb1a29309d919f55
olajideIfe <olajideifedolapo1@gmail.com>
2026-06-04 01:23:35 -0700

    feat: add transactiom category filtering

--- src/App.jsx
+++ src/App.jsx
@@ -14,12 +14,17 @@ const App = () => {
   });
 
   const [transactions, setTransactions] = useState(() => {
-    const saved = localStorage.getItem("transactions");
+    const savedTransactions = localStorage.getItem("transactions");
 
-    return saved ? JSON.parse(saved) : [];
+    return savedTransactions ? JSON.parse(savedTransactions) : [];
   });
 
   const [search, setSearch] = useState("");
+  const [categoryFilter, setCategoryFilter] = useState("All");
+
+  useEffect(() => {
+    localStorage.setItem("transactions", JSON.stringify(transactions));
+  }, [transactions]);
 
   const handleChange = (e) => {
     setFormData({
@@ -63,6 +68,17 @@ const App = () => {
 
   const balance = income - expenses;
 
+  const filteredTransactions = transactions.filter((transaction) => {
+    const matchesSearch = transaction.title
+      .toLowerCase()
+      .includes(search.toLowerCase());
+
+    const matchesCategory =
+      categoryFilter === "All" || transaction.category === categoryFilter;
+
+    return matchesSearch && matchesCategory;
+  });
+
   return (
     <div className="p-6">
       <h1 className="text-4xl font-bold mb-8">💰 MoneyFlow</h1>
@@ -76,6 +92,32 @@ const App = () => {
       />
 
       <SearchBar search={search} setSearch={setSearch} />
+      <div
+        className="
+flex
+gap-2
+flex-wrap
+mb-6
+"
+      >
+        <button
+          onClick={() => setCategoryFilter("All")}
+          className=" px-4 py-2 rounded-full bg-white shadow hover:shadow-lg transition " >
+          All
+        </button>
+
+        <button onClick={() => setCategoryFilter("Salary")}className=" px-4 py-2 rounded-full bg-white shadow hover:shadow-lg transition " >Salary</button>
+
+        <button onClick={() => setCategoryFilter("Food")}className=" px-4 py-2 rounded-full bg-white shadow hover:shadow-lg transition " >Food</button>
+
+        <button onClick={() => setCategoryFilter("Transport")} className=" px-4 py-2 rounded-full bg-white shadow hover:shadow-lg transition " >
+          Transport
+        </button>
+
+        <button onClick={() => setCategoryFilter("Bills")}className=" px-4 py-2 rounded-full bg-white shadow hover:shadow-lg transition " >Bills</button>
+
+        <button onClick={() => setCategoryFilter("Shopping")}className=" px-4 py-2 rounded-full bg-white shadow hover:shadow-lg transition " >Shopping</button>
+      </div>
 
       {transactions.length === 0 ? (
         <div
@@ -96,7 +138,7 @@ const App = () => {
         </div>
       ) : (
         <div className="grid md:grid-cols-2 gap-6 mt-8">
-          {transactions.map((transaction) => (
+          {filteredTransactions.map((transaction) => (
             <TransactionCard
               key={transaction.id}
               transaction={transaction}
